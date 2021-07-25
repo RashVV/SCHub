9bb4ec22a894362b391b2103dca9c138905d6db4
RashVV <vadim.ratushnyak@gmail.com>
2021-07-25 22:44:29 +0300

    New components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Header from './Header/Heder.jsx'
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <header className="App-header">     
       <div>
         <img src='https://img1.pnghut.com/14/16/1/DMcXD5PPsX/new-year-fishing-tackle-baits-lures-santa-claus-christmas-tree.jpg' width="540">
---
+++ src/Header/Heder.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './header.module.css'
+
+const Header = () =>{
+    return (
+        <div className='headernew'>
+        <a>Create Letter</a>
+        <a>How it work</a>
+        <a>Donate</a>
+        <a>Contact</a>
+        </div>
+    )
+}
+
+export default Header;
