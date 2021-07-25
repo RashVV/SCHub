e901b72a71b1e5baa0fd28aaa489eb044dd61613
RashVV <vadim.ratushnyak@gmail.com>
2021-07-25 21:51:35 +0300

    Add SClaus and text

--- src/App.js
+++ src/App.js
@@ -5,20 +5,15 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="App-header">     
+      <div>
+        <img src='https://img1.pnghut.com/14/16/1/DMcXD5PPsX/new-year-fishing-tackle-baits-lures-santa-claus-christmas-tree.jpg' width="540">
+
+        </img>
+        <p>Send letter to Santa Claus</p>
+      </div>
       </header>
+
     </div>
   );
 }
