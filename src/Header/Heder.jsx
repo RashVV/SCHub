caab7b8cd79c3cea1e3553fca67911a547f800aa
RashVV <vadim.ratushnyak@gmail.com>
2021-07-30 15:49:55 +0300

    add new menu

--- src/Header/Heder.jsx
+++ src/Header/Heder.jsx
@@ -1,15 +1,36 @@
 import React from 'react';
-import './header.module.css'
+import Button from '@material-ui/core/Button';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
 
-const Header = () =>{
-    return (
-        <div className='headernew'>
-        <div>Create Letter</div>
-        <div>How it work</div>
-        <div>Donate</div>
-        <div>Contact</div>
-        </div>
-    )
+export default function Header() {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  return (
+    <div>
+      <Button aria-controls="simple-menu" aria-haspopup="true" onClick={handleClick}>
+        Open Menu
+      </Button>
+      <Menu
+        id="simple-menu"
+        anchorEl={anchorEl}
+        keepMounted
+        open={Boolean(anchorEl)}
+        onClose={handleClose}
+      >
+        <MenuItem onClick={handleClose}>Create Letter</MenuItem>
+        <MenuItem onClick={handleClose}>How it work</MenuItem>
+        <MenuItem onClick={handleClose}>Donate</MenuItem>
+        <MenuItem onClick={handleClose}>Contact</MenuItem>
+      </Menu>
+    </div>
+  );
 }
-
-export default Header;
