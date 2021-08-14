4d5ce472544ca84c2eadadfe0da00b87963f8e9e
Vlad Ratushniak <vadim.ratushnyak@gmail.com>
2021-08-14 23:39:47 +0300

    add valus in form to consollog

--- src/ComposedTextField.js
+++ src/ComposedTextField.js
@@ -1,10 +1,11 @@
-import React, {useState} from 'react';
+import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import FormControl from '@material-ui/core/FormControl';
 import FormHelperText from '@material-ui/core/FormHelperText';
 import Input from '@material-ui/core/Input';
 import InputLabel from '@material-ui/core/InputLabel';
 import Button from '@material-ui/core/Button';
+import { useForm } from 'react-hook-form';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -15,43 +16,28 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 export default function ComposedTextField() {
-  const [name, setName] = React.useState('Your name');
-  const [age, setAge] = React.useState('Your age');
-  const [deals, setDeals] = React.useState('Good deals');
-  const classes = useStyles();
-
-  const handleChange = (event) => {
-    setName(event.target.value);
-  };
-  const handleChangeAge = (event) => {
-    setAge(event.target.value);
-  };
-  const handleChangeDeals = (event) => {
-    setDeals(event.target.value);
-  };
-  function SaveUserData(){
-    var name = document.getElementById('component-name').value
-    console.log("users value is:" + name);
-  }
+  const {register, handleSubmit } = useForm();
+  const onSubmit = data => console.log(data);
 
   return (
-    <form className={classes.root} noValidate autoComplete="off">
+    <form  noValidate autoComplete="off" onSubmit={handleSubmit(onSubmit)}>
       <FormControl>
         <InputLabel htmlFor="component-name">My Name is___</InputLabel>
-        <Input id="component-name" value={name} onChange={handleChange} />
+        <Input {...register("name")} />
         <FormHelperText id="component-helper-text">Write your Name.</FormHelperText>
       </FormControl>
       <FormControl>
         <InputLabel htmlFor="component-age">Iam ___years old</InputLabel>
-        <Input id="component-age" value={age} onChange={handleChangeAge} />
+        <Input {...register("age")} />
         <FormHelperText id="component-helper-text">Write How old are you?</FormHelperText>
       </FormControl>
       <FormControl>
         <InputLabel htmlFor="component-deals">Your good deals</InputLabel>
-        <Input id="component-deals" value={deals} onChange={handleChangeDeals} />
+        <Input {...register("deals")} />
         <FormHelperText id="component-helper-text">What good deals you make this year.</FormHelperText>
       </FormControl>
-      <Button variant="outlined" onClick={() => this.SaveUserData()} color="secondary">Create and Sent</Button>
+      <imput type="submit" />
+      <Button variant="outlined" type="submit" color="secondary">Create and Sent</Button>
       </form>
        );
 }
